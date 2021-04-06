d44e3be68787740752d8710b5a1c23b38417b7b4
DaniilAnd <lisichkindan@yandex.ru>
2021-04-06 07:01:34 +0300

    Добавьте файлы проекта.

---
+++ Class/Calc.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Storonkin.CalculationPaints.Class
+{
+    static class Calc
+    {
+
+        private static double Res;
+        // Метод вычисления стоимости покраски
+        public static double Result(double Square,int Layer,int Price,double SquareWindow)
+        {
+            double temp = (((Square - SquareWindow) * Layer) / ((8 + 14) / 2)) * Price;
+            Res = temp;
+            return Res;
+        }
+        // Метод вычисления количества банок
+        public static int ResCount(double Square, int Layer, double SquareWindow) 
+        {
+            int temp = Convert.ToInt32(((Square - SquareWindow) * Layer) / ((8 + 14) / 2));
+            return temp + 1 ;
+
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,77 @@
+﻿using Storonkin.CalculationPaints.Class;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Storonkin.CalculationPaints
+{
+    /// <summary>
+    /// Логика взаимодействия для Главной формы
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+       
+        public MainWindow()
+        {
+            InitializeComponent();
+            
+        }
+
+        
+
+
+
+
+
+        double Temp;
+        // Кнопка Результат
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            // Square - площадь стен Layer - кол-во слоёв Price - цена за банку SquareWindow - площадь окон
+            // Площадь окон должна быть меньше площади стен
+            if (double.Parse(Square.Text) > double.Parse(SquareWindow1.Text))
+            {
+                // Вывод стоимость краски для покраски стен
+                 Temp = Math.Round(Calc.Result(double.Parse(Square.Text), int.Parse(Layer.Text), int.Parse(Price.Text), double.Parse(SquareWindow1.Text)), 2);
+                 OutPrice.Content = Temp;
+                // Вывод количества банок с запасом для покупки
+                OutCount.Content = Calc.ResCount(double.Parse(Square.Text), int.Parse(Layer.Text), double.Parse(SquareWindow1.Text)).ToString();
+                WarringLabel.Content = "";
+                //Проверка типа краски и изменение цены
+                //if (ComboBox1.SelectedItem.ToString() == "Акриловая")
+                   // OutPrice.Content = Temp;
+                //else if (ComboBox1.SelectedItem.ToString() == "Латексная")
+                    //OutPrice.Content = Temp * 1.10;
+               // else if (ComboBox1.SelectedItem.ToString() == "Акрилатная")
+                    //OutPrice.Content = Temp * 1.15;
+                //else if (ComboBox1.SelectedItem.ToString() == "Водно-диспансерная")
+                    //OutPrice.Content = Temp * 1.20;
+
+            
+
+            }
+            // Вывод ошибкок
+            else if ((double.Parse(Layer.Text) < 1))
+                WarringLabel.Content = "Кол-во слоев не может быть меньше 1 или отрицательные";
+            else
+                WarringLabel.Content = "Площадь окон не может быть больше площади стен";
+        }
+
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+    }
+}
